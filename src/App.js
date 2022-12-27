58b3f308a8d69f79a8f471be39df848165df20af
bhavanarajakram <bhavanarajak78@gmail.com>
2022-12-27 23:00:49 +0530

    added files > added component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Button from './component/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>hhh</h1>
+      <Button/>
     </div>
   );
 }
---
+++ src/component/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <button style={{background:"red"}}>submit</button>
+    </div>
+  )
+}
+
+export default Button
